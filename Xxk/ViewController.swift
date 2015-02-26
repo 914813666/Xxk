fe4a69cdb8571e0eb2a856e7fd50080be8aafb61
914813666 <com.qzp>
2015-02-26 14:13:12 +0800

    Initial Commit

---
+++ Xxk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xxk
+//
+//  Created by qzp on 15/2/26.
+//  Copyright (c) 2015年 qzp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
